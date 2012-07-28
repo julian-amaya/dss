b229dab18d55692e437f01061ef1c9ed714cee3e
David Avellaneda <g.avella@gmail.com>
2012-07-28 11:53:59 -0500

    primera version del template

--- dss/settings.py
+++ dss/settings.py
@@ -70,6 +70,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1]+['_res']),
 )
 
 # List of finder classes that know how to find static files in
@@ -109,6 +110,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1]+['templates']),
 )
 
 INSTALLED_APPS = (
