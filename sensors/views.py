40e30a9b379b688533ce76156a5d91e8eb58a432
David Avellaneda <g.avella@gmail.com>
2012-07-28 14:45:14 -0500

    segunda version

--- sensors/views.py
+++ sensors/views.py
@@ -1,5 +1,5 @@
 # Create your views here.
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.template.context import RequestContext
 from django.shortcuts import get_list_or_404, get_object_or_404, render_to_response, redirect
 
@@ -9,7 +9,17 @@ def home(request):
     data = {}
 
     valores = ValorSensor.objects.all()
-    p = Paginator(valores, 2)
+    page = request.GET.get('page')
+    paginator = Paginator(valores, 2)
+    try:
+        sensores = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        sensores = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        sensores = paginator.page(paginator.num_pages)
+    data['sensores_paginator'] = paginator
+    data['sensores'] = sensores
 
-    data['paginator'] = p
     return render_to_response('index.html', data,context_instance=RequestContext(request)) 
