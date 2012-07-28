a1e0bac29d9ad90342c1f0c0b4325ab9ab1aa02b
Julian Amaya <julian@monoku.com>
2012-07-28 13:01:02 -0500

    agrego requirements

--- sensors/task.py
+++ sensors/task.py
@@ -1,18 +1,20 @@
-from celery	import task
+from celery import task
 import requests
 import datetime
+
+from sensors.models import ValorSensor
 
 
 @task
 def SensorSave():
-	res = requests.get('http://deathstartsensor.herokuapp.com/?star=django')
-	a=res.content
-	b=a.split(',')
-	fecha=b[0]
-	sensores=b[1:]
-	#print sensores
-	fecha = fecha[:-6]
-	#print fecha
-	#print datetime.datetime.strptime(fecha, "%Y-%m-%dT%H:%M:%S")
-	for i,s in enumerate(sensores):
-		ValorSensor.objects.create(fecha_hora=fecha,valor=s) 
+    res = requests.get('http://deathstartsensor.herokuapp.com/?star=django')
+    a = res.content
+    b = a.split(',')
+    fecha = b[0]
+    sensores = b[1:]
+    #print sensores
+    fecha = fecha[:-6]
+    #print fecha
+    fecha = datetime.datetime.strptime(fecha, "%Y-%m-%dT%H:%M:%S")
+    for i, s in enumerate(sensores):
+        ValorSensor.objects.create(fecha_hora=fecha, valor=s)
